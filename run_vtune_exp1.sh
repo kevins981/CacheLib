99f21ef7454a58f8456af28899ff23a146470a40
kevins981 <kevinsong981@gmail.com>
2022-11-18 11:11:05 -0500

    Updated vtune version

--- run_vtune_exp1.sh
+++ run_vtune_exp1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESULT_DIR="/ssd1/songxin8/thesis/cache/vtune/all_sweep_exp1/"
+RESULT_DIR="/ssd1/songxin8/thesis/cache/vtune/exp1_64GBDIMMs/"
 WORKING_DIR="/ssd1/songxin8/thesis/cache/CacheLib/"
 CONFIG_DIR="${WORKING_DIR}/cachelib/cachebench/test_configs/ecosys/"
 
@@ -18,25 +18,31 @@ run_vtune () {
   CONFIG=$1
   MEMNODE=$2
 
-  LD_PRELOAD=/usr/lib/x86_64-linux-gnu/debug/libstdc++.so.6.0.28 /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune \
+  LD_PRELOAD=/usr/lib/x86_64-linux-gnu/debug/libstdc++.so.6.0.28 /opt/intel/oneapi/vtune/latest/bin64/vtune \
       -collect hotspots -start-paused -data-limit=10000 -result-dir ${RESULT_DIR}/${config}_hotspot_node${MEMNODE} \
       --app-working-dir=${WORKING_DIR} \
       -- /usr/bin/numactl --membind=${MEMNODE} --cpunodebind=0 \
       ./opt/cachelib/bin/cachebench --json_test_config $CONFIG_DIR/$config/config.json --progress=5
 
-  LD_PRELOAD=/usr/lib/x86_64-linux-gnu/debug/libstdc++.so.6.0.28 /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune \
+  clean_cache
+
+  LD_PRELOAD=/usr/lib/x86_64-linux-gnu/debug/libstdc++.so.6.0.28 /opt/intel/oneapi/vtune/latest/bin64/vtune \
       -collect memory-access -start-paused -knob sampling-interval=10 -knob analyze-mem-objects=true \
       -knob mem-object-size-min-thres=256 -knob analyze-openmp=true -data-limit=10000 \
       -result-dir ${RESULT_DIR}/${config}_memacc_node${MEMNODE} --app-working-dir=${WORKING_DIR} \
       -- /usr/bin/numactl --membind=${MEMNODE} --cpunodebind=0 \
       ./opt/cachelib/bin/cachebench --json_test_config $CONFIG_DIR/$config/config.json --progress=5
 
-  LD_PRELOAD=/usr/lib/x86_64-linux-gnu/debug/libstdc++.so.6.0.28 /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune \
+  clean_cache
+  
+  LD_PRELOAD=/usr/lib/x86_64-linux-gnu/debug/libstdc++.so.6.0.28 /opt/intel/oneapi/vtune/latest/bin64/vtune \
       -collect uarch-exploration -start-paused -knob sampling-interval=10 -knob collect-memory-bandwidth=true \
       -data-limit=10000 \
       -result-dir ${RESULT_DIR}/${config}_uarch_node${MEMNODE} --app-working-dir=${WORKING_DIR} \
       -- /usr/bin/numactl --membind=${MEMNODE} --cpunodebind=0 \
       ./opt/cachelib/bin/cachebench --json_test_config $CONFIG_DIR/$config/config.json --progress=5
+
+  clean_cache
 
 }
 
@@ -53,7 +59,7 @@ for config in "${CONFIG_LIST[@]}"
 do
   clean_cache
   run_vtune $config 0
-  clean_cache
-  run_vtune $config 1
+  #clean_cache
+  #run_vtune $config 1
 done
 
